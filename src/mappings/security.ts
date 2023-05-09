53dc57d253fbe3e64c153215c74a393abdb49754
Kallol Borah <kallol.borah@gmail.com>
2023-05-09 19:45:47 +0530

    fixed handlers for corporate resolutions and snapshots

--- src/mappings/security.ts
+++ src/mappings/security.ts
@@ -9,9 +9,9 @@ import {
   } from "../../generated/schema";
 
 export function handleResolutions(event: Resolution): void {
-    let resolutions = Resolutions.load(event.params.resolution.concat('-').concat(event.transaction.hash.toHexString()));
+    let resolutions = Resolutions.load(event.params.security.toHexString().concat('-').concat(event.transaction.hash.toHexString()));
     if(resolutions==null){
-        let resolution = event.params.resolution;
+        let resolution = event.params.security.toHexString();
         let resolutions = new Resolutions(resolution);
         resolutions.security = event.params.security.toHexString();
         resolutions.recordDate = event.params.recordDate.toI32();
@@ -29,9 +29,9 @@ export function handleResolutions(event: Resolution): void {
 }
 
 export function handleSchedulingSnapshot(event: SnapshotSchedule): void {
-    let oldSnapshot = Snapshots.load(event.params.oldTime.toHexString().concat('-').concat(event.transaction.hash.toHexString()));
+    let oldSnapshot = Snapshots.load(event.params.security.toHexString().concat('-').concat(event.transaction.hash.toHexString()));
     if(oldSnapshot==null){
-        let ot = event.params.oldTime.toHexString();
+        let ot = event.params.security.toHexString();
         let oldSnapshot = new Snapshots(ot);
         oldSnapshot.security = event.params.security.toHexString();
         oldSnapshot.oldTime = event.params.oldTime.toI32();
@@ -47,10 +47,16 @@ export function handleSchedulingSnapshot(event: SnapshotSchedule): void {
 }
 
 export function handleUnschedulingSnapshot(event: SnapshotUnschedule): void {
-    let newSnapshot = Snapshots.load(event.params.time.toHexString().concat('-').concat(event.transaction.hash.toHexString()));
+    let newSnapshot = Snapshots.load(event.params.security.toHexString().concat('-').concat(event.transaction.hash.toHexString()));
     if(newSnapshot==null){
-        let timeset = event.params.time.toHexString();
+        let timeset = event.params.security.toHexString();
         let newSnapshot = new Snapshots(timeset);
+        newSnapshot.security = event.params.security.toHexString();
+        newSnapshot.oldTime = event.params.time.toI32();
+        newSnapshot.newTime = 0;
+        newSnapshot.save();
+    }
+    else{
         newSnapshot.security = event.params.security.toHexString();
         newSnapshot.oldTime = event.params.time.toI32();
         newSnapshot.newTime = 0;
